1a3415d1374d630a7e3fd83e536b8f7f0b1bb4a1
V / Lou du Poitou <101828845+Lou-du-Poitou@users.noreply.github.com>
2024-05-29 22:58:00 +0200

    Create FileCipher.cmd

---
+++ FileCipher.cmd
@@ -0,0 +1,1 @@
+python pyinstaller --onefile "C:\Users\YOUR_PATH\FileCipher.py" --icon "C:\Users\YOUR_PATH\FileCipher.png" --noconsole --argv-emulation --add-data="C:\Users\YOUR_PATH\FileCipher.ico;icon"
